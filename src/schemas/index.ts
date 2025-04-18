555d05b40596ddf75f7b824c6a9b3b3d97763dd8
joealdersonstrachan <joealdersonstrachan@strachan.email>
2025-04-18 15:59:54 +0100

    feat: initial commit

---
+++ src/schemas/index.ts
@@ -0,0 +1,13 @@
+import * as dolomiteSchemasModule from '@datr.tech/cargo-router-validation-schemas-dolomite';
+import * as freightSchemasModule from '@datr.tech/cargo-router-validation-schemas-freight';
+import * as granul8SchemasModule from '@datr.tech/cargo-router-validation-schemas-granul8';
+import * as personaSchemasModule from '@datr.tech/cargo-router-validation-schemas-persona';
+import * as procSchemasModule from '@datr.tech/cargo-router-validation-schemas-proc';
+import * as entitySchemasModule from '@datr.tech/cargo-router-validation-schemas-entity';
+
+export const dolomiteSchemas = dolomiteSchemasModule;
+export const entitySchemas = entitySchemasModule;
+export const freightSchemas = freightSchemasModule;
+export const granul8Schemas = granul8SchemasModule;
+export const personaSchemas = personaSchemasModule;
+export const procSchemas = procSchemasModule;
